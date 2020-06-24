3a5383c9bcb0fedd319f72871a2ad9cbbe06bde7
jingquansun <67076484+jingquansun@users.noreply.github.com>
2020-06-24 12:16:26 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,55 @@
+"""
+Python Web Development Techdegree
+Project 1 - Number Guessing Game
+--------------------------------
+
+For this first project we will be using Workspaces. 
+
+NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
+
+"""
+
+import random
+
+def high_score(current, new):
+  if new <= current:
+    return new
+  else:
+    return current
+
+def start_game():
+  current_highscore = 10
+  while True:
+    answer = random.randint(1, 10)
+    
+    guess = input("Hello! Welcome to the guessing game. The current high score is {}. Please guess a number between 1 and 10:   ".format(current_highscore))
+    try:
+      guess = int(guess)
+    except ValueError as err:
+      print("This is an invalid input. {} Please start over.".format(err))
+      start_game()
+    else:
+      tries = 1
+      while guess != answer:
+        if guess > 10 or guess < 1:
+          guess = input("This is not a valid number. The number is between 1 and 10, please guess again:   ")
+          guess = int(guess)
+        if guess > answer:
+          guess = input("It's lower! Please guess again:   ")
+          guess = int(guess)
+          tries += 1
+        elif guess < answer:
+          guess = input("It's higher! Please guess again:   ")
+          guess = int(guess)
+          tries += 1
+        
+      current_highscore = high_score(current_highscore, tries)
+      print("You got it! It took you {} tries!".format(tries))
+      restart = input("The current high score is {}! The game is over, would you like to play again?\nYes/No:  ".format(current_highscore))
+      if restart.lower() == "yes":
+        continue
+      else:
+        print("Thanks for playing!")
+        break
+    
+start_game()
